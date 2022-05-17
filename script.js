724e743c28b94504e200e2c2d4ba7bc3cf7a006a
Vladyslav <prostoandrei9@gmail.com>
2022-05-17 11:57:39 +0300

    1.0.0
    it works :)

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log("hello world!");
+let activity = document.getElementById('activity');
+let newActivityBtn = document.getElementById('newActivity');
+newActivityBtn.onclick = newTask;
+
+
+async function newTask(){
+    let url = "http://www.boredapi.com/api/activity";
+    let response = await fetch(url);
+    let result = await response.json();
+
+    console.log(result);
+
+    activity.innerHTML = result.activity;
+    
+}
+newTask();
+
+
