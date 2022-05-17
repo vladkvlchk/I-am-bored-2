8a9e69ff87ac08f4ece98516a9ffc2e846779e27
Vladyslav <prostoandrei9@gmail.com>
2022-05-17 12:33:53 +0300

    1.1.0
    added description

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let activity = document.getElementById('activity');
-let newActivityBtn = document.getElementById('newActivity');
+let activity = document.getElementById('activity'),
+type = document.getElementById('type'),
+price = document.getElementById('price'),
+accessibility = document.getElementById('accessibility'),
+participants = document.getElementById('participants'),
+newActivityBtn = document.getElementById('newActivity');
+
 newActivityBtn.onclick = newTask;
 
 
@@ -8,9 +13,12 @@ async function newTask(){
     let response = await fetch(url);
     let result = await response.json();
 
-    console.log(result);
-
     activity.innerHTML = result.activity;
+    type.innerHTML = `type: ${result.type}`;
+    price.innerHTML = `price: ${result.price * 100} lvl`;
+    accessibility.innerHTML = `accessibility: ${+result.accessibility * 100}%`;
+    participants.innerHTML = `participants: ${result.participants}`;
+
     
 }
 newTask();
